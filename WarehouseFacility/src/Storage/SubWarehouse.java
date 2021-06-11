2f91be262fafb704f1a31ec1a97f04bda18f662e
aynader <83070878+aynader@users.noreply.github.com>
2021-06-11 18:14:00 +0200

    Bare bones Concept
    - Implemented a basic logic based approach to the problem.

--- WarehouseFacility/src/Storage/SubWarehouse.java
+++ WarehouseFacility/src/Storage/SubWarehouse.java
@@ -2,111 +2,93 @@ package Storage;
 
 import java.io.Console;
 import java.util.ArrayList;
+
 public class SubWarehouse {
-	
-	
+
 	int maxCapacity;
-	ArrayList<Integer> itemWeights = new ArrayList<>() ;
-	ArrayList<Integer> itemProfits = new ArrayList<>(); 					
+	ArrayList<Integer> itemWeights = new ArrayList<>();
+	ArrayList<Integer> itemProfits = new ArrayList<>();
 	int maxChosenProfit;
 	int availableItems;
-	
-	
-	
-	
-	
-	public void optimizeSelection()
-	{
-		//TODO: Create a method that optimizes the selection of items in order to produce the most profit and make the most out of the storage space.
-		
-		//Your code here
-		//You're allowed to use any other pre-defined methods.
-		
-		
-	
-        
- 
-        maxChosenProfit =  0; //Update with your answer.
-        
-        //TODO: Enter your GUC mail here
-        String email = "";
-        System.out.println("Email: " + email);
-    }
-		
-	
-	
-	public static int max(int a, int b)
-	    {
-	      return (a > b) ? a : b;
-	    }
-	
-	
-	
-	
+
+	public int optimizeHelper(int NumOfItems, int Weight) {
+
+		if (NumOfItems == 0)
+			return 0;
+		if (Weight == 0)
+			return 0;
+		if (itemWeights.get(NumOfItems - 1) > Weight) {
+			return optimizeHelper(NumOfItems - 1, Weight);
+		} else {
+			return max(
+					itemProfits.get(NumOfItems - 1)
+							+ optimizeHelper(NumOfItems - 1, Weight - itemWeights.get(NumOfItems - 1)),
+					optimizeHelper(NumOfItems - 1, Weight));
+		}
+	}
+
+	public void optimizeSelection() {
+
+		// Implementing a Prolog-like approach, we can check recursively for the maximum profit,
+		// by recursively calling a resulting maxProfit and finally checking if the
+		// value is higher with or without the item at said index
+
+		maxChosenProfit = optimizeHelper(itemWeights.size(), maxCapacity);
+
+		String email = "amr.nader@student.guc.edu.eg";
+		System.out.println("Email: " + email);
+	}
+
+	public static int max(int a, int b) {
+		return (a > b) ? a : b;
+	}
 
 	public static void main(String[] args) {
-		
+
 		SubWarehouse SW = new SubWarehouse();
-		
-		//populating the ArrayLists   
+
+		// populating the ArrayLists
 		SW.itemWeights.add(56);
 		SW.itemWeights.add(59);
-		SW.itemWeights.add(80);       // itemWeights = {56 , 59 , 80 , 64, 75 , 17};
+		SW.itemWeights.add(80); // itemWeights = {56 , 59 , 80 , 64, 75 , 17};
 		SW.itemWeights.add(64);
 		SW.itemWeights.add(75);
-		SW.itemWeights.add(17);	
-		
-		
-		SW.itemProfits.add(50);                                
+		SW.itemWeights.add(17);
+
+		SW.itemProfits.add(50);
 		SW.itemProfits.add(50);
 		SW.itemProfits.add(64);
-		SW.itemProfits.add(46);		  // itemProfits = {50 , 50 , 64 , 46 , 50 , 5}; 
+		SW.itemProfits.add(46); // itemProfits = {50 , 50 , 64 , 46 , 50 , 5};
 		SW.itemProfits.add(50);
 		SW.itemProfits.add(5);
 
-		SW.maxCapacity = 190;          	   //maximum warehouse capacity = 190 weight unit
-		SW.availableItems = 6; 			   //available items are 6
-		
-		
-		
-		
-		
-		//Step 1 :
-		//TODO: Call your optimization method here and Print your results.
-		//Expected Results : Populate maxChosenProfit with the total final Profit
-		
-		SW.optimizeSelection();
-		System.out.println( "Max Profit " + SW.maxChosenProfit); //optimum answer should be maxChosenProfit = 150
+		SW.maxCapacity = 190; // maximum warehouse capacity = 190 weight unit
+		SW.availableItems = 6; // available items are 6
 
-		
-		
-		
-		//Step 2 :
-		//TODO: Try out your solution on more complex datasets that are used in practical world
-		
-		
-		//Uncomment to load the dataset before re-running your method again
-		
-		
-		
-		/*DataLoader.loadArray("kp100.txt", SW.itemWeights, SW.itemProfits, SW.maxCapacity);
+		// Step 1 :
+		// TODO: Call your optimization method here and Print your results.
+		// Expected Results : Populate maxChosenProfit with the total final Profit
+
+		SW.optimizeSelection();
+		System.out.println("Max Profit " + SW.maxChosenProfit); // optimum answer should be maxChosenProfit = 150
+
+		// Step 2 :
+		// TODO: Try out your solution on more complex datasets that are used in
+		// practical world
+
+		// Uncomment to load the dataset before re-running your method again
+
+		/*
+		 * DataLoader.loadArray("kp100.txt", SW.itemWeights, SW.itemProfits,
+		 * SW.maxCapacity);
 		 * 
-		SW.maxCapacity= 49877;
-		SW.availableItems = 10000 ;
-		SW.optimizeSelection();
-		System.out.println( "Max Profit " + SW.maxChosenProfit); // should be Max Profit 594669   
-		*
-		*
-		*/
+		 * SW.maxCapacity= 49877; SW.availableItems = 10000 ; SW.optimizeSelection();
+		 * System.out.println( "Max Profit " + SW.maxChosenProfit); // should be Max
+		 * Profit 594669
+		 *
+		 *
+		 */
 
-
-		
-		
-		
-		
-		
 	}
 
-	
-	
 }
