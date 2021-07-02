90f7910ad4597f2a293eff3c79d07845475cf363
KareemEhab <39467484+KareemEhab@users.noreply.github.com>
2021-07-02 22:00:21 +0200

    Solved with DP using Bottom-Up approach

--- WarehouseFacility/src/Storage/SubWarehouse.java
+++ WarehouseFacility/src/Storage/SubWarehouse.java
@@ -32,19 +32,19 @@ public class SubWarehouse {
 		public int DP(int n, int cap)
 		{
 			int i, w;
-	        int K[][] = new int[n+1][cap+1];
-	 
-	        for (i = 0; i <= n; i++)
-	            for (w = 0; w <= cap; w++)
-	            {
-	                if (i == 0 || w == 0)
-	                    K[i][w] = 0;
-	                else if (itemWeights.get(i-1) <= w)
-	                    K[i][w] = max(itemProfits.get(i-1) + K[i-1][w - itemWeights.get(i - 1)], K[i-1][w]);
-	                else
-	                    K[i][w] = K[i-1][w];
-	            }
-	        return K[n][cap];
+			int K[][] = new int[n+1][cap+1];
+
+			for (i = 0; i <= n; i++)
+			    for (w = 0; w <= cap; w++)
+			    {
+				if (i == 0 || w == 0)
+				    K[i][w] = 0;
+				else if (itemWeights.get(i-1) <= w)
+				    K[i][w] = max(itemProfits.get(i-1) + K[i-1][w - itemWeights.get(i - 1)], K[i-1][w]);
+				else
+				    K[i][w] = K[i-1][w];
+			    }
+			return K[n][cap];
 		}
 		
 	
